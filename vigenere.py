0b3cb3dd0163558bf87eca2763519f9819cb8383
Michał Pawlenka <Michal.pawlenka@gsuite.us.edu.pl>
2026-04-15 10:08:02 +0200

    Test vigenere

--- vigenere.py
+++ vigenere.py
@@ -3,7 +3,7 @@ def vigenere(text, key):
     ret = ""
     for letter in text:
         shift = ord(key[key_index % len(key)]) - ord('A')
-        new_letter = (ord(letter) - ord('A') + shift) % (ord('Z')-ord('A')+1)
+        new_letter = (ord(letter) - ord('A') + shift) % (ord('Z')-ord('A'))
         new_letter += ord('A')
         ret += chr(new_letter)
         key_index += 1
