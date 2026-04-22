aeaddfbe4e04811121ba3ca00dddf44ed6382668
Rafał Palka <raflikpalka@gmail.com>
2026-04-22 09:16:07 +0200

    dodano funkcję gen_random_key

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ from cezar import szyfr_cezara
 from cezar_brute_force import cezar_brute_force
 from vigenere import *
 from decrypt_vigenere import *
+import secrets
+import string
+
+def gen_random_key(param: int):
+    return ''.join(secrets.choice(string.ascii_uppercase + string.digits) for _ in range(param))
 
 print(szyfr_cezara("Alan", 3))
 cezar_brute_force("Dodq")
